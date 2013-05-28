52ab53ee2db2a5516dd911ee1a3d2c8d7b325de8
patrick <github@onyxbits.de>
2013-05-28 13:12:27 +0200

    Launcher icons updated

---
+++ preproduction/exporter.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+inkscape -w 48 -e ../res/drawable-mdpi/ic_launcher.png  ic_launcher.svg
+inkscape -w 144 -e ../res/drawable-xxhdpi/ic_launcher.png  ic_launcher.svg
+inkscape -w 72 -e ../res/drawable-hdpi/ic_launcher.png  ic_launcher.svg
+inkscape -w 96 -e ../res/drawable-xhdpi/ic_launcher.png  ic_launcher.svg
