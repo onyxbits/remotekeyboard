30be8f215e27ae736038bbbd509cd5176ab8b9f0
patrick <github@onyxbits.de>
2013-06-02 15:06:51 +0200

    Added keyboard switch widget

--- preproduction/exporter.sh
+++ preproduction/exporter.sh
@@ -3,3 +3,8 @@ inkscape -w 48 -e ../res/drawable-mdpi/ic_launcher.png  ic_launcher.svg
 inkscape -w 144 -e ../res/drawable-xxhdpi/ic_launcher.png  ic_launcher.svg
 inkscape -w 72 -e ../res/drawable-hdpi/ic_launcher.png  ic_launcher.svg
 inkscape -w 96 -e ../res/drawable-xhdpi/ic_launcher.png  ic_launcher.svg
+
+inkscape -w 48 -e ../res/drawable-mdpi/ic_widget.png  ic_widget.svg
+inkscape -w 144 -e ../res/drawable-xxhdpi/ic_widget.png  ic_widget.svg
+inkscape -w 72 -e ../res/drawable-hdpi/ic_widget.png  ic_widget.svg
+inkscape -w 96 -e ../res/drawable-xhdpi/ic_widget.png  ic_widget.svg
