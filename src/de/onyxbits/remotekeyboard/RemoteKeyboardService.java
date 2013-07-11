6eb501345569d9ba72460a6c67f749710643ec58
patrick <github@onyxbits.de>
2013-07-11 02:13:58 +0200

    Cleanup

--- src/de/onyxbits/remotekeyboard/RemoteKeyboardService.java
+++ src/de/onyxbits/remotekeyboard/RemoteKeyboardService.java
@@ -95,8 +95,6 @@ public class RemoteKeyboardService extends InputMethodService implements
 
 	@Override
 	public View onCreateInputView() {
-		// RemoteKeyboardView ret = (RemoteKeyboardView) getLayoutInflater().
-		// inflate(R.layout.input,null);
 		KeyboardView ret = new KeyboardView(this, null);
 		ret.setKeyboard(new Keyboard(this, R.xml.keyboarddef));
 		ret.setOnKeyboardActionListener(this);
